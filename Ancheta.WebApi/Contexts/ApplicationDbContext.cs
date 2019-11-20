7315ef9d79a8992a7798733844085187e28eb6e7
Mihai Stan <stanmihai17cs@gmail.com>
2019-11-20 17:49:24 +0100

    Add application database context

---
+++ Ancheta.WebApi/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+using Ancheta.Model.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ancheta.WebApi.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Poll> Polls { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Poll>()
+                .HasMany(p => p.Answers)
+                .WithOne(a => a.OwnerPoll)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Answer>()
+                .HasOne(a => a.OwnerPoll)
+                .WithMany(p => p.Answers);
+        }
+
+    }
+}
