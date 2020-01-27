158f829d686ec7c9d27d5122d9403708854db7f3
Mihai Stan <stanmihai17cs@gmail.com>
2020-01-27 11:41:49 +0100

    Add tests project and tests for PollService

---
+++ Ancheta.WebApi.Tests/PollServiceTests.cs
@@ -0,0 +1,72 @@
+using System;
+using Ancheta.Model.Services;
+using Xunit;
+
+namespace Ancheta.WebApi.Tests
+{
+    public class PollServiceTests
+    {
+
+        private readonly PollService _pollService;
+
+        public PollServiceTests()
+        {
+            _pollService = new PollService();
+        }
+
+        [Fact]
+        public void GenerateScretCode_WithValidLength_ShouldReturn_PassHashPair()
+        {
+            // Arrange
+            const int length = 10;
+
+            // Act
+            var (password, hash) = _pollService.GenerateSecretCode(length);
+
+            // Assert
+            Assert.NotEmpty(password);
+            Assert.NotEmpty(hash);
+        }
+
+        [Fact]
+        public void GenerateScretCode_WithInvalidLength_ShouldThrow_ArgumentException()
+        {
+            // Arrange
+            const int length = 0;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => {
+                var (password, hash) = _pollService.GenerateSecretCode(length);
+            });
+        }
+
+        [Fact]
+        public void IsPasswordValid_WithValidPassHashPair_ShouldReturn_True()
+        {
+            // Arange
+            const int passLength = 10;
+            var (password, hash) = _pollService.GenerateSecretCode(passLength);
+
+            // Act
+            bool isValid = _pollService.IsPasswordValid(password, hash);
+
+            // Assert
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void IsPasswordValid_WithInvalidPassHashPair_ShouldReturn_False()
+        {
+            // Arange
+            const int passLength = 10;
+            var (password, hash) = _pollService.GenerateSecretCode(passLength);
+
+            // Act
+            bool isValid = _pollService.IsPasswordValid(password.Substring(0, passLength / 2), hash);
+
+            // Assert
+            Assert.False(isValid);
+        }
+
+    }
+}
